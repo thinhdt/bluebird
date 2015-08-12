919cb9fc53458f8646209eb761efcd8d19e62eae
mac <mac@Thinhdts-MacBook-Pro.local>
2015-08-12 14:46:06 +0700

    commit code

--- bluebird/ViewController.m
+++ bluebird/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIImageView *bird;
+}
 
 @end
 
@@ -16,12 +19,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    [self drawJungle];
+    [self addBird];
+    [self flyUpAndDown];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (void) drawJungle {
+    UIImageView *backgroud = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"jungle.jpg"]];
+    backgroud.frame = self.view.bounds;
+    
+    backgroud.contentMode = UIViewContentModeScaleAspectFill;
+    [self.view addSubview:backgroud];
 }
+
+- (void) addBird {
+    bird = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 110, 68)];
+    bird.animationImages = @[ [UIImage imageNamed:@"bird0.png"],
+                              [UIImage imageNamed:@"bird1.png"],
+                              [UIImage imageNamed:@"bird2.png"],
+                              [UIImage imageNamed:@"bird3.png"],
+                              [UIImage imageNamed:@"bird4.png"],
+                              [UIImage imageNamed:@"bird5.png"],
+                             ];
+    
+    bird.animationRepeatCount = -1; // forever loop
+    bird.animationDuration = 1; // khoang thoi gian chay giua annimation . time to run 6 birds
+    
+    [self.view addSubview:bird];
+    [bird startAnimating];
+    
+}
+
+- (void) flyUpAndDown {
+    bird.transform = CGAffineTransformIdentity; // reset all the before transform
+    [UIView animateWithDuration:5 animations:^{
+        // the bird is at the bottom screen
+        bird.center = CGPointMake(self.view.bounds.size.width, self.view.bounds.size.height);
+    } completion:^(BOOL finished) {
+        
+        bird.transform = CGAffineTransformConcat(
+                                                 CGAffineTransformMakeScale (-1, 1),
+                                                 CGAffineTransformMakeRotation (M_PI_4));
+        
+        [UIView animateWithDuration:5 animations:^{
+            bird.center = CGPointMake(0, 0);
+        } completion:^(BOOL finished) {
+            [self flyUpAndDown];
+        }];
+    }];
+    
+    
+}
+
 
 @end
